60e6141330a849f9cb238e54884259eb52d6d207
EstivnQR <126968413+EstivenQR@users.noreply.github.com>
2025-01-29 03:07:24 -0600

    Se agrega Generar Reporte y un filtro para la tabla
    Se genera un PDF según el departamento seleccionado y se agrega también un filtro para la tabla

--- Plantilla_Access/Interfaces/usuarioDAO.php
+++ Plantilla_Access/Interfaces/usuarioDAO.php
@@ -1,12 +1,23 @@
 <?php
 interface UsuarioDAO
 {
+
     public function getAllUsers();
+
+    public function getUsersByDepartment($id_departamento); // Nuevo método para filtrar por departamento
+
+    
     public function AgregarUsuario($id_departamento, $id_rol, $nombre, 
     $apellido, $fecha_nacimiento, $fecha_ingreso, $cargo, 
     $correo_electronico, $username, $password, $numero_telefonico, 
     $direccion_imagen, $sexo, $estado_civil, $fechacreacion, 
-    $usuariocreacion, $fechamodificacion, $usuariomodificacion);
+    $usuariocreacion, $fechamodificacion, $usuariomodificacion); 
+
 
 }
+
+
+
+
+
 ?>
