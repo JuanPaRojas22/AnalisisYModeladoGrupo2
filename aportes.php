b506c69d5b3e217cabe797e6bd2b8efe0f937937
Esteven Quirós <126968413+EstivenQR@users.noreply.github.com>
2025-06-12 02:12:03 -0600

    Update aportes.php

--- aportes.php
+++ aportes.php
@@ -2,11 +2,31 @@
 session_start();
 include 'template.php';
 // Conexión a la base de datos
-$mysqli = new mysqli("localhost", "root", "", "gestionempleados");
+// Parámetros de conexión
+$host = "accespersoneldb.mysql.database.azure.com";
+$user = "adminUser";
+$password = "admin123+";
+$dbname = "gestionEmpleados";
+$port = 3306;
 
-if ($mysqli->connect_error) {
-    die("Conexión fallida: " . $mysqli->connect_error);
+// Ruta al certificado CA para validar SSL
+$ssl_ca = '/home/site/wwwroot/certs/BaltimoreCyberTrustRoot.crt.pem';
+
+// Inicializamos mysqli
+$conn = mysqli_init();
+
+// Configuramos SSL
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+mysqli_options($conn, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);
+
+
+// Intentamos conectar usando SSL (con la bandera MYSQLI_CLIENT_SSL)
+if (!$conn->real_connect($host, $user, $password, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
+    die("Error de conexión: " . mysqli_connect_error());
 }
+
+// Establecemos el charset
+mysqli_set_charset($conn, "utf8mb4");
 
 // Obtener los aportes fijas (últimas 5)
 $query_aport = "SELECT * FROM aportes ORDER BY id_usuario DESC LIMIT 5";
