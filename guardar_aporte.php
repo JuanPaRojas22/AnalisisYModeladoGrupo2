df8ee53c0d19f0da0b4521d54fb7337c9220d7f4
Esteven Quirós <126968413+EstivenQR@users.noreply.github.com>
2025-06-12 02:05:56 -0600

    Update guardar_aporte.php

--- guardar_aporte.php
+++ guardar_aporte.php
@@ -18,13 +18,31 @@ if (empty($aporte)) {
     exit;
 }
 
-// Conexión a la base de datos
-$conexion = new mysqli("localhost", "root", "", "gestionempleados");
+// Parámetros de conexión
+$host = "accespersoneldb.mysql.database.azure.com";
+$user = "adminUser";
+$password = "admin123+";
+$dbname = "gestionEmpleados";
+$port = 3306;
 
-if ($conexion->connect_error) {
-    echo json_encode(['success' => false, 'message' => 'Error de conexión']);
-    exit;
+// Ruta al certificado CA para validar SSL
+$ssl_ca = '/home/site/wwwroot/certs/BaltimoreCyberTrustRoot.crt.pem';
+
+// Inicializamos mysqli
+$conn = mysqli_init();
+
+// Configuramos SSL
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+mysqli_options($conn, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);
+
+
+// Intentamos conectar usando SSL (con la bandera MYSQLI_CLIENT_SSL)
+if (!$conn->real_connect($host, $user, $password, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
+    die("Error de conexión: " . mysqli_connect_error());
 }
+
+// Establecemos el charset
+mysqli_set_charset($conn, "utf8mb4");
 
 // Obtener id_usuario desde username
 $stmtUser = $conexion->prepare("SELECT id_usuario FROM usuario WHERE username = ?");
