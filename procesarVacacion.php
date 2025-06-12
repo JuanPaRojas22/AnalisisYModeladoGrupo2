0300bd28e3592c9007f062a6458c38a062254f67
Esteven Quirós <126968413+EstivenQR@users.noreply.github.com>
2025-06-12 02:03:54 -0600

    Update procesarVacacion.php

--- procesarVacacion.php
+++ procesarVacacion.php
@@ -12,12 +12,31 @@ if(isset($_GET['id']) && isset($_GET['accion'])){
     // Verifica si la accion es aprobar o rechazar
     if($accion == 'aprobar'){
         // Conectar a la base de datos
-        $conn = new mysqli("localhost", "root", "", "GestionEmpleados");
+// Parámetros de conexión
+$host = "accespersoneldb.mysql.database.azure.com";
+$user = "adminUser";
+$password = "admin123+";
+$dbname = "gestionEmpleados";
+$port = 3306;
 
-        // Verificar conexión
-        if ($conn->connect_error) {
-            die("Error de conexión: " . $conn->connect_error);
-        }
+// Ruta al certificado CA para validar SSL
+$ssl_ca = '/home/site/wwwroot/certs/BaltimoreCyberTrustRoot.crt.pem';
+
+// Inicializamos mysqli
+$conn = mysqli_init();
+
+// Configuramos SSL
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+mysqli_options($conn, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);
+
+
+// Intentamos conectar usando SSL (con la bandera MYSQLI_CLIENT_SSL)
+if (!$conn->real_connect($host, $user, $password, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
+    die("Error de conexión: " . mysqli_connect_error());
+}
+
+// Establecemos el charset
+mysqli_set_charset($conn, "utf8mb4");
 
         $query = "SELECT diasTomado, id_usuario FROM vacacion WHERE id_vacacion = ?";
         $stmt = $conn->prepare($query);
