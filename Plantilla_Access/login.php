4850cf3e77b422651d5c1c15ee2a418a5d10ff32
EstivnQR <126968413+EstivenQR@users.noreply.github.com>
2025-06-11 19:11:41 -0600

    Update login.php

--- Plantilla_Access/login.php
+++ Plantilla_Access/login.php
@@ -1,6 +1,6 @@
 <?php
 // Se hace la conexión a la base de datos
-$conn = new mysqli("accespersoneldb.mysql.database.azure.com", "adminUser", "admin123+", "GestionEmpleados");
+$conn = new mysqli("127.0.0.1", "adminUser", "admin123+", "GestionEmpleados");
 mysqli_set_charset($conn, "utf8mb4");
 
 // Se valida la conexión a la base de datos
