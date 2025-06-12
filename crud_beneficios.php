e9d5ba76eb5caf37d228f8aa91845ce2120021de
Esteven Quirós <126968413+EstivenQR@users.noreply.github.com>
2025-06-12 02:11:25 -0600

    Update crud_beneficios.php

--- crud_beneficios.php
+++ crud_beneficios.php
@@ -1,10 +1,30 @@
 <?php
 session_start();
-$conexion = new mysqli("localhost", "root", "", "gestionempleados");
+// Parámetros de conexión
+$host = "accespersoneldb.mysql.database.azure.com";
+$user = "adminUser";
+$password = "admin123+";
+$dbname = "gestionEmpleados";
+$port = 3306;
 
-if ($conexion->connect_error) {
-    die(json_encode(["success" => false, "message" => "Error de conexión: " . $conexion->connect_error]));
+// Ruta al certificado CA para validar SSL
+$ssl_ca = '/home/site/wwwroot/certs/BaltimoreCyberTrustRoot.crt.pem';
+
+// Inicializamos mysqli
+$conn = mysqli_init();
+
+// Configuramos SSL
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+mysqli_options($conn, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);
+
+
+// Intentamos conectar usando SSL (con la bandera MYSQLI_CLIENT_SSL)
+if (!$conn->real_connect($host, $user, $password, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
+    die("Error de conexión: " . mysqli_connect_error());
 }
+
+// Establecemos el charset
+mysqli_set_charset($conn, "utf8mb4");
 
 // Validar si la acción está definida
 $action = $_POST['action'] ?? $_GET['action'] ?? '';
