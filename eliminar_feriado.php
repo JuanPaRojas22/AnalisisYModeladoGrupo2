8a1d924a98ac547367570414b4461e86a8b1138c
Esteven Quirós <126968413+EstivenQR@users.noreply.github.com>
2025-06-12 02:08:39 -0600

    Update eliminar_feriado.php

--- eliminar_feriado.php
+++ eliminar_feriado.php
@@ -1,10 +1,29 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "GestionEmpleados");
-mysqli_set_charset($conn, "utf8mb4");
+// Parámetros de conexión
+$host = "accespersoneldb.mysql.database.azure.com";
+$user = "adminUser";
+$password = "admin123+";
+$dbname = "gestionEmpleados";
+$port = 3306;
 
-if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
+// Ruta al certificado CA para validar SSL
+$ssl_ca = '/home/site/wwwroot/certs/BaltimoreCyberTrustRoot.crt.pem';
+
+// Inicializamos mysqli
+$conn = mysqli_init();
+
+// Configuramos SSL
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+mysqli_options($conn, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);
+
+
+// Intentamos conectar usando SSL (con la bandera MYSQLI_CLIENT_SSL)
+if (!$conn->real_connect($host, $user, $password, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
+    die("Error de conexión: " . mysqli_connect_error());
 }
+
+// Establecemos el charset
+mysqli_set_charset($conn, "utf8mb4");
 
 $id = $_POST['id_fecha'];
 
