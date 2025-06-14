63a328dd901f420ca3285e024d001b01f25b8d08
Esteven Quirós <126968413+EstivenQR@users.noreply.github.com>
2025-06-14 15:44:49 -0600

    Update ver_historial_cambios.php

--- ver_historial_cambios.php
+++ ver_historial_cambios.php
@@ -151,10 +151,28 @@ include 'template.php';
         <section class="wrapper site-min-height">
 
             <?php
-            $conn = new mysqli("localhost", "root", "", "gestionempleados");
-            if ($conn->connect_error) {
-                die("Error de conexión: " . $conn->connect_error);
-            }
+// Parámetros de conexión
+$host = "accespersoneldb.mysql.database.azure.com";
+$user = "adminUser";
+$password = "admin123+";
+$dbname = "gestionEmpleados";
+$port = 3306;
+
+// Ruta al certificado CA para validar SSL
+$ssl_ca = '/home/site/wwwroot/certs/BaltimoreCyberTrustRoot.crt.pem';
+
+// Inicializamos mysqli
+$conn = mysqli_init();
+
+// Configuramos SSL
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+mysqli_options($conn, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);
+
+
+// Intentamos conectar usando SSL (con la bandera MYSQLI_CLIENT_SSL)
+if (!$conn->real_connect($host, $user, $password, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
+    die("Error de conexión: " . mysqli_connect_error());
+}
 
             $sql = "SELECT 
                         hc.id_historial_cargos, 
