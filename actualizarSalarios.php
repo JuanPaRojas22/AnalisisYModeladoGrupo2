2cefd8f92243f0697d748669536361df5770f0a9
403leo <128949367+403leo@users.noreply.github.com>
2025-06-14 19:24:42 -0600

    Update actualizarSalarios.php

--- actualizarSalarios.php
+++ actualizarSalarios.php
@@ -1,6 +1,32 @@
 <?php
-require 'conexion.php';
+// Conexión a la base de datos
+// Parámetros de conexión
+$host = "accespersoneldb.mysql.database.azure.com";
+$user = "adminUser";
+$password = "admin123+";
+$dbname = "gestionEmpleados";
+$port = 3306;
+
+// Ruta al certificado CA para validar SSL
+$ssl_ca = '/home/site/wwwroot/certs/BaltimoreCyberTrustRoot.crt.pem';
+
+// Inicializamos mysqli
+$conn = mysqli_init();
+
+// Configuramos SSL
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+mysqli_options($conn, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);
+
+
+// Intentamos conectar usando SSL (con la bandera MYSQLI_CLIENT_SSL)
+if (!$conn->real_connect($host, $user, $password, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
+    die("Error de conexión: " . mysqli_connect_error());
+}
+
+// Establecemos el charset
+mysqli_set_charset($conn, "utf8mb4");
 session_start();
+require 'template.php';
 if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
     header("Location: login.php");
     exit;
@@ -59,212 +85,7 @@ $mensaje = "";
 <body>
 
     <section id="container">
-        <!-- **********************************************************************************************************************************************************
-      TOP BAR CONTENT & NOTIFICATIONS
-      *********************************************************************************************************************************************************** -->
-        <!--header start-->
-        <header class="header black-bg">
-            <div class="sidebar-toggle-box">
-                <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
-            </div>
-            <a href="index.php" class="logo"><b>Acces Perssonel</b></a>
-            <div class="nav notify-row" id="top_menu">
-                <!-- Notifications -->
-                <ul class="nav top-menu">
-                    <!-- Tasks Dropdown -->
-                    <li class="dropdown">
-                        <a data-toggle="dropdown" class="dropdown-toggle" href="#">
-                            <i class="fa fa-tasks"></i>
-                            <span class="badge bg-theme">4</span>
-                        </a>
-                        <ul class="dropdown-menu extended tasks-bar">
-                            <div class="notify-arrow notify-arrow-green"></div>
-                            <li>
-                                <p class="green">You have 4 pending tasks</p>
-                            </li>
-                            <!-- Example Task -->
-                            <li>
-                                <a href="#">
-                                    <div class="task-info">
-                                        <div class="desc">DashGum Admin Panel</div>
-                                        <div class="percent">40%</div>
-                                    </div>
-                                    <div class="progress progress-striped">
-                                        <div class="progress-bar progress-bar-success" style="width: 40%"></div>
-                                    </div>
-                                </a>
-                            </li>
-                            <!-- More tasks -->
-                        </ul>
-                    </li>
-                    <!-- Messages Dropdown -->
-                    <li id="header_inbox_bar" class="dropdown">
-                        <a data-toggle="dropdown" class="dropdown-toggle" href="#">
-                            <i class="fa fa-envelope-o"></i>
-                            <span class="badge bg-theme">5</span>
-                        </a>
-                        <ul class="dropdown-menu extended inbox">
-                            <div class="notify-arrow notify-arrow-green"></div>
-                            <li>
-                                <p class="green">You have 5 new messages</p>
-                            </li>
-                            <!-- Example Message -->
-                            <li>
-                                <a href="#">
-                                    <span class="photo"><img alt="avatar" src="assets/img/ui-zac.jpg"></span>
-                                    <span class="subject">
-                                        <span class="from">Zac Snider</span>
-                                        <span class="time">Just now</span>
-                                    </span>
-                                    <span class="message">Hi mate, how is everything?</span>
-                                </a>
-                            </li>
-                            <!-- More messages -->
-                        </ul>
-                    </li>
-
-                    <li id="header_profile_bar" class="dropdown">
-                        <a data-toggle="dropdown" class="dropdown-toggle" href="#">
-                            <i class="fa fa-user"></i>
-                            <span class="badge bg-theme">1</span>
-                        </a>
-                        <ul class="dropdown-menu extended inbox">
-                            <div class="notify-arrow notify-arrow-green"></div>
-                            <li>
-                                <p class="green">User Profile</p>
-                            </li>
-                            <li><a href="profile.php"><i class="fa fa-cogs"></i> Edit Profile</a></li>
-                            <li><a href="settings.php"><i class="fa fa-cogs"></i> Settings</a></li>
-                            <li><a href="login.php"><i class="fa fa-sign-out"></i> Logout</a></li>
-                        </ul>
-                    </li>
-
-                </ul>
-                <div class="top-menu">
-                    <?php if (isset($_GET['login']) && $_GET['login'] == 'success' && isset($_GET['username'])): ?>
-                        <ul class="nav pull-right top-menu">
-                            <li>
-                                <h3>Bienvenido, <?php echo htmlspecialchars($username); ?>!</h3>
-                            </li>
-                        </ul>
-                    <?php elseif (isset($_GET['login']) && $_GET['login'] == 'error'): ?>
-                        <ul class="nav pull-right top-menu">
-                            <li>
-                                <h3>Error en el inicio de sesión. Por favor, inténtelo de nuevo.</h3>
-                            </li>
-                        </ul>
-                    <?php endif; ?>
-                </div>
-
-        </header>
-        <!--header end-->
-
-        <!-- **********************************************************************************************************************************************************
-      MAIN SIDEBAR MENU
-      *********************************************************************************************************************************************************** -->
-        <!--sidebar start-->
-        <aside>
-            <div id="sidebar" class="nav-collapse ">
-                <!-- sidebar menu start-->
-                <ul class="sidebar-menu" id="nav-accordion">
-
-                    <div id="sidebar" class="nav-collapse ">
-                        <!-- sidebar menu start-->
-                        <ul class="sidebar-menu" id="nav-accordion">
-
-                            <p class="centered"><a href="profile.html"><img src="assets/img/ui-sam.jpg"
-                                        class="img-circle" width="60"></a></p>
-                            <h5 class="centered">Bienvenido, <?php echo $_SESSION['username']; ?>!</h5>
-
-                            <li class="mt">
-                                <a class="active" href="index.html">
-                                    <i class="fa fa-dashboard"></i>
-                                    <span>Dashboard</span>
-                                </a>
-                            </li>
-
-                            <li class="sub-menu">
-                                <a href="javascript:;">
-                                    <i class="fa fa-desktop"></i>
-                                    <span>UI Elements</span>
-                                </a>
-                                <ul class="sub">
-                                    <li><a href="general.html">General</a></li>
-                                    <li><a href="buttons.html">Buttons</a></li>
-                                    <li><a href="panels.html">Panels</a></li>
-                                </ul>
-                            </li>
-
-
-                            <li class="sub-menu">
-                                <a href="javascript:;">
-                                    <i class="fa fa-cogs"></i>
-                                    <span>Components</span>
-                                </a>
-                                <ul class="sub">
-                                    <li><a href="calendar.html">Calendar</a></li>
-                                    <li><a href="gallery.html">Gallery</a></li>
-                                    <li><a href="todo_list.html">Todo List</a></li>
-                                </ul>
-                            </li>
-                            <li class="sub-menu">
-                                <a href="javascript:;">
-                                    <i class="bi bi-person-fill-gear"></i>
-                                    <span>Administracion</span>
-                                </a>
-                                <ul class="sub">
-                                    <li><a href="VerPlanilla.php"><i
-                                                class="bi bi-journal-bookmark"></i><span>Planilla</span></a></li>
-                                    <li><a href="MostrarUsuarios.php"><i
-                                                class="bi bi-person-lines-fill"></i><span>Usuarios</span></a>
-                                    </li>
-                                    <li><a href="registrar_cambio_puesto.php">Registrar Cambio de Puesto</a></li>
-                                    <li><a href="ver_historial_cambios.php">Historial de Cambios</a></li>
-
-                                </ul>
-                            </li>
-
-                            </li>
-                            <li class="sub-menu">
-                                <a href="javascript:;">
-                                    <i class="fa fa-tasks"></i>
-                                    <span>Forms</span>
-                                </a>
-                                <ul class="sub">
-                                    <li><a href="form_component.html">Form Components</a></li>
-                                </ul>
-                            </li>
-                            <li class="sub-menu">
-                                <a href="javascript:;">
-                                    <i class="fa fa-th"></i>
-                                    <span>Data Tables</span>
-                                </a>
-                                <ul class="sub">
-                                    <li><a href="basic_table.html">Basic Table</a></li>
-                                    <li><a href="responsive_table.html">Responsive Table</a></li>
-                                </ul>
-                            </li>
-                            <li class="sub-menu">
-                                <a href="javascript:;">
-                                    <i class=" fa fa-bar-chart-o"></i>
-                                    <span>Charts</span>
-                                </a>
-                                <ul class="sub">
-                                    <li><a href="morris.html">Morris</a></li>
-                                    <li><a href="chartjs.html">Chartjs</a></li>
-                                </ul>
-                            </li>
-
-                        </ul>
-                        <!-- sidebar menu end-->
-                    </div>
-        </aside>
-        <!--sidebar end-->
-
-        <!-- **********************************************************************************************************************************************************
-      MAIN CONTENT
-      *********************************************************************************************************************************************************** -->
-        <!--main content start-->
+        
         <section id="main-content">
             <section class="wrapper site-min-height">
 
