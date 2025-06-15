2172cf1bee33addb148963584874aee7f492c3fb
403leo <leonardomadsanchez@gmail.com>
2025-06-15 14:23:17 -0600

    Cambio Editar Usuarios
    Cambio Editar Usuarios

--- Plantilla_Access/MostrarUsuarios.php
+++ Plantilla_Access/MostrarUsuarios.php
@@ -46,37 +46,14 @@ if ($id_departamento == 'all') {
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="">
-  <meta name="author" content="Dashboard">
-  <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
-  <!-- Bootstrap core CSS -->
-  <link href="assets/css/bootstrap.css" rel="stylesheet">
-  <!--external css-->
-  <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
-  <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
-  <link rel="stylesheet" type="text/css" href="assets/css/zabuto_calendar.css">
-  <link rel="stylesheet" type="text/css" href="assets/js/gritter/css/jquery.gritter.css" />
-  <link rel="stylesheet" type="text/css" href="assets/lineicons/style.css">
-
-  <!-- Custom styles for this template -->
-  <link href="assets/css/style.css" rel="stylesheet">
-  <link href="assets/css/style-responsive.css" rel="stylesheet">
 
   <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <style>
-        td, div {
-            color: black !important;
-        }
-    </style>
+
 </head>
 
 
@@ -166,7 +143,7 @@ if ($id_departamento == 'all') {
                             </p>
                             <!-- Botones dentro del card-body -->
                             <div class="d-flex justify-content-center gap-4">
-                                <a href="profile.php?id=<?= $user['id_usuario'] ?>"
+                                <a href="profileUser.php?id=<?= $user['id_usuario'] ?>"
                                     class="btn btn-outline-primary btn-sm rounded-pill">
                                     <i class="bi bi-pencil-square"></i>
                                 </a>
