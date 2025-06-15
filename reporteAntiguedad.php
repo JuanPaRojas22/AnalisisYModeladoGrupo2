8de285f4eb4e951d87b12c87478653e9b223662f
403leo <128949367+403leo@users.noreply.github.com>
2025-06-14 20:13:46 -0600

    Update reporteAntiguedad.php

--- reporteAntiguedad.php
+++ reporteAntiguedad.php
@@ -1,6 +1,31 @@
 <?php
+// Conexión a la base de datos
+// Parámetros de conexión
+$host = "accespersoneldb.mysql.database.azure.com";
+$user = "adminUser";
+$password = "admin123+";
+$dbname = "gestionEmpleados";
+$port = 3306;
+
+// Ruta al certificado CA para validar SSL
+$ssl_ca = '/home/site/wwwroot/certs/BaltimoreCyberTrustRoot.crt.pem';
+
+// Inicializamos mysqli
+$conn = mysqli_init();
+
+// Configuramos SSL
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+mysqli_options($conn, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);
+
+
+// Intentamos conectar usando SSL (con la bandera MYSQLI_CLIENT_SSL)
+if (!$conn->real_connect($host, $user, $password, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
+    die("Error de conexión: " . mysqli_connect_error());
+}
+
+// Establecemos el charset
+mysqli_set_charset($conn, "utf8mb4");
 session_start();
-require 'conexion.php';
 include 'template.php';
 // Consulta para calcular la antigüedad y obtener los beneficios
 $query = "
