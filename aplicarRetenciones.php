d04ce962477f43def242dbc89963f43a7c62376e
EstivnQR <126968413+EstivenQR@users.noreply.github.com>
2025-07-22 03:39:37 -0600

    Update aplicarRetenciones.php

--- aplicarRetenciones.php
+++ aplicarRetenciones.php
@@ -245,7 +245,7 @@ JOIN Usuario u ON p.id_usuario = u.id_usuario";
               <div class="card" style="border-radius: 15px; padding: 30px; box-shadow: 0 4px 10px rgb(255, 255, 255);">
                 <div class="card-body">
                   <h2 class="text-center mb-4">Aplicar Deducción Salarial</h2>
-
+                  <a href="AgregarDeduccionesextra.php" class="btn btn-success">Agregar Deducción Extra</a>
                   <form action="" method="POST" class="form-horizontal">
                     <!-- Select Employee -->
                     <div class="form-group">
@@ -276,7 +276,6 @@ JOIN Usuario u ON p.id_usuario = u.id_usuario";
                     <!-- Buttons -->
                     <div class="form-group text-center">
                       <button type="submit" class="btn btn-success">Aplicar Deducción</button>
-                      <a href="AgregarDeduccionesextra.php" class="btn btn-info">Agregar<br>Deducción Extra</a>
                       <a href="VerPlanilla.php" class="btn btn-info">Volver</a>
                     </div>
                   </form>
