d97beda71a0a203a44ab7cf587975638dfd2d1e9
EstivnQR <126968413+EstivenQR@users.noreply.github.com>
2025-07-27 03:22:28 -0600

    Update MostrarUsuarios.php

--- MostrarUsuarios.php
+++ MostrarUsuarios.php
@@ -119,7 +119,7 @@ if ($id_departamento == 'all') {
             <div class="row gx-3">
                 <?php foreach ($users as $user): ?>
                     <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
-                        <div class="card h-100 shadow-sm border-0">
+                        <div class="card h-100 shadow-sm border-0 p-3">
                             <div class="card-body">
                                 <h5 class="card-title fw-bold">
                                     <?= htmlspecialchars($user['nombre']) . " " . htmlspecialchars($user['apellido']) ?>
