4cf7e2c1b3c0fc7efc87af3401eee2e8d4e16d15
EstivnQR <126968413+EstivenQR@users.noreply.github.com>
2025-07-28 22:56:48 -0600

    cambio

--- template.php
+++ template.php
@@ -320,15 +320,14 @@ if (isset($_SESSION['id_usuario'])) {
         <button class="boton-flotante" onclick="abrirModal()">✨ Hacer un aporte</button>
         <!-- Modal aporte -->
         <div id="modalAporteContainer">
-            <div id="miModal" class="modal">
+            <div class="modal">
                 <div class="modal-contenido">
                     <span class="cerrar" onclick="cerrarModal()">&times;</span>
                     <h2>Haz tu aporte</h2>
                     <form id="enviarAporte">
-                        <div><input type="text" value="<?= $_SESSION['nombre']; ?>" readonly></div>
-                        <div><textarea id="aporte" name="aporte" placeholder="Escribe tu aporte..." required></textarea>
-                        </div>
-                        <div><button type="submit" class="enviar">Enviar</button></div>
+                        <input type="text" value="<?= $_SESSION['nombre']; ?>" readonly>
+                        <textarea id="aporte" name="aporte" placeholder="Escribe tu aporte..." required></textarea>
+                        <button type="submit" class="enviar">Enviar</button>
                     </form>
                 </div>
             </div>
