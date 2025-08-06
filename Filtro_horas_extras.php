58b0c3b9addc5f40e60a29c080edf4754874fdff
EstivnQR <126968413+EstivenQR@users.noreply.github.com>
2025-08-06 17:10:43 -0600

    Update Filtro_horas_extras.php

--- Filtro_horas_extras.php
+++ Filtro_horas_extras.php
@@ -205,20 +205,16 @@ $total_paginas = ceil($total_registros / $registros_por_pagina);
             <div style="text-align: center;">
                 <?php if (!empty($data)): ?>
 
-                    <<form action="reporte_horas_extra.php" method="post">
-                        <input type="hidden" name="usuario" value="<?php echo htmlspecialchars($usuario); ?>">
-                        <input type="hidden" name="departamento" value="<?php echo htmlspecialchars($departamento); ?>">
-
-
+                    <form action="reporte_horas_extra.php" method="post">
+                        <input type="hidden" name="usuario"
+                            value="<?php echo htmlspecialchars($_SESSION['filtro_usuario'] ?? ''); ?>">
+                        <input type="hidden" name="departamento"
+                            value="<?php echo htmlspecialchars($_SESSION['filtro_departamento'] ?? ''); ?>">
                         <button class="btn" type="submit" name="exportar_pdf">
                             <i class="bi bi-file-earmark-arrow-down-fill"></i> Exportar PDF
                         </button>
-
-
-
-
-                        </form>
-                    <?php endif; ?>
+                    </form>
+                <?php endif; ?>
             </div>
         <?php endif; ?>
 
