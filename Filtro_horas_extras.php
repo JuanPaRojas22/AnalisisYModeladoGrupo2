75f5a172ccfc814e760c1e3047bd49c6d3228e72
EstivnQR <126968413+EstivenQR@users.noreply.github.com>
2025-08-06 08:57:25 -0600

    Update Filtro_horas_extras.php

--- Filtro_horas_extras.php
+++ Filtro_horas_extras.php
@@ -172,19 +172,20 @@ $total_paginas = ceil($total_registros / $registros_por_pagina);
                     <a href="VerPlanilla.php" class="btn btn-secondary">Devolver</a>
                     <button class="btn" type="submit" name="filtrar" id="btnFiltrar">Filtrar</button>
             </form>
-
-            <?php if (!empty($data)): ?>
-                <form action="reporte_horas_extra.php" method="post">
-                    <input type="hidden" name="usuario" value="<?php echo $usuario; ?>">
-                    <input type="hidden" name="departamento" value="<?php echo $departamento; ?>">
-                    <button class="btn" type="submit" name="exportar_pdf">
-                        <i class="bi bi-file-earmark-arrow-down-fill"></i> Exportar PDF
-                    </button>
-
-                <?php endif; ?>
-            </form>
-
         <?php endif; ?>
+
+        <?php if (!empty($data)): ?>
+            <form action="reporte_horas_extra.php" method="post">
+                <input type="hidden" name="usuario" value="<?php echo $usuario; ?>">
+                <input type="hidden" name="departamento" value="<?php echo $departamento; ?>">
+                <button class="btn" type="submit" name="exportar_pdf">
+                    <i class="bi bi-file-earmark-arrow-down-fill"></i> Exportar PDF
+                </button>
+
+            <?php endif; ?>
+        </form>
+
+
 
 
 
