9a10d9d9d2b0883f305083c98990ca3453c1712b
EstivnQR <126968413+EstivenQR@users.noreply.github.com>
2025-08-06 23:58:01 -0600

    Update aplicarRetenciones.php

--- aplicarRetenciones.php
+++ aplicarRetenciones.php
@@ -7,6 +7,8 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
   exit;
 }
 
+require "template.php";
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
