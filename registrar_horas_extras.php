b80910aef36292d0897c0801f4be51d0da7bef07
Esteven Quirós <126968413+EstivenQR@users.noreply.github.com>
2025-08-12 09:40:13 -0600

    Update registrar_horas_extras.php

--- registrar_horas_extras.php
+++ registrar_horas_extras.php
@@ -398,12 +398,12 @@ if (isset($_FILES['archivo_excel']) && $_FILES['archivo_excel']['error'] == 0) {
 
             if (!empty($nombre_empleado)) {
                 $query_emp = "SELECT planilla.id_usuario, planilla.id_planilla, planilla.salario_base, planilla.salario_neto 
-              FROM planilla 
-              INNER JOIN usuario ON planilla.id_usuario = usuario.id_usuario
-              WHERE LOWER(CONCAT(TRIM(usuario.nombre), ' ', TRIM(usuario.apellido))) = LOWER(?)";
-
-                $stmt = $conn->prepare($query_emp);
-                $stmt->bind_param("si", $nombre_empleado, $departamento_admin);
+                    FROM planilla 
+                      INNER JOIN usuario ON planilla.id_usuario = usuario.id_usuario
+                      WHERE LOWER(CONCAT(TRIM(usuario.nombre), ' ', TRIM(usuario.apellido))) = LOWER(?) 
+                      AND usuario.id_departamento = ?";
+                    $stmt = $conn->prepare($query_emp);
+                    $stmt->bind_param("si", $nombre_empleado, $departamento_admin);
 
                 if ($stmt->execute()) {
                     $stmt->bind_result($id_usuario, $id_planilla, $salario_base, $salario_neto);
